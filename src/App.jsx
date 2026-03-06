708ae7e77847afcbd0f26966ae6113705ae8df98
danyangelova <daniela.ant.ang@gmail.com>
2026-03-06 23:04:41 +0200

    Add tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { useEffect } from "react";
+import { fetchTextFile } from "./services/fetchTextFile";
+import { parseCSV } from "./utils/parseCSV";
+import { mapperTests } from "./tests.js";
 
-function App() {
+export default function App() {
+   useEffect(() => {
+      async function test() {
+         const text = await fetchTextFile("/data/players.csv");
+         // console.log(text);
+         const rows = parseCSV(text);
+         // console.log(rows);
+         // console.log("first row:", rows[0]);
+      }
 
-  return (
-    <>
-      <h1>Hii</h1>
-    </>
-  )
+      test().catch(console.error);
+   }, []);
+
+   useEffect(() => {
+      mapperTests(); 
+   }, []);
 }
-
-export default App
